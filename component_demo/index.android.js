b1a5b0ab4a714aed729a7805ea00770bcdd23e67
J_Knight_ <ssjlife0111@163.com>
2017-07-11 08:13:35 +0800

    add [17]. advertise_demo

--- component_demo/index.android.js
+++ component_demo/index.android.js
@@ -1,7 +1,6 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * @flow
  */
 
 import React, { Component } from 'react';
@@ -9,19 +8,44 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  Image
+  View
 } from 'react-native';
 
-var LoginView = require('./loginView');
-
-class LoginViewDemo extends Component{
-   render(){
-     return(
-         <LoginView/>
-     );
-   }
+class FScrollViewDemo1 extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.android.js
+        </Text>
+        <Text style={styles.instructions}>
+          Shake or press menu button for dev menu
+        </Text>
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
 
-AppRegistry.registerComponent('component_demo', () => LoginViewDemo);
+AppRegistry.registerComponent('FScrollViewDemo1', () => FScrollViewDemo1);
