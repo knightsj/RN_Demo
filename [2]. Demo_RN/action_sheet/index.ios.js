0b798280aa43b68f711f7834dd35fe95fc70e445
J_Knight_ <ssjlife0111@163.com>
2017-09-29 12:28:50 +0800

    add action_sheet demo

---
+++ [2]. Demo_RN/action_sheet/index.ios.js
@@ -0,0 +1,92 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+    TouchableOpacity
+} from 'react-native';
+
+import ActionSheet from './ActionSheet'
+
+const selectedArr = ["拍照", "图库"];
+
+export default class action_sheet extends Component {
+
+  constructor(props) {
+    super(props);
+    this.dialog = ActionSheet;
+    this.showAlertSelected = this.showAlertSelected.bind(this);
+    this.callbackSelected = this.callbackSelected.bind(this);
+  }
+
+  showAlertSelected(){
+    this.dialog.show("请选择照片", selectedArr, '#333333', this.callbackSelected);
+  }
+
+  callbackSelected(i){
+    switch (i){
+      case 0: // 拍照
+        this.takePhoto();
+        break;
+      case 1: // 图库
+        this.pickMultiple();
+        break;
+    }
+  }
+
+  takePhoto(){
+
+  }
+
+  pickMultiple(){
+
+  }
+
+  show(){
+    alert('show')
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={()=>this.showAlertSelected()}>
+          <Text style={styles.welcome}>
+            Welcome to React Native***
+          </Text>
+        </TouchableOpacity>
+        <ActionSheet ref={(dialog)=>{
+          this.dialog = dialog;
+        }} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('action_sheet', () => action_sheet);
