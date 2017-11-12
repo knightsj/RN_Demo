8d0e4fdd07984d627b5b148ff88e1ad315ba021f
J_Knight_ <ssjlife0111@163.com>
2017-11-12 22:50:36 +0800

    add webview component

---
+++ [6]. Component/webview_component/WebViewPage.js
@@ -0,0 +1,77 @@
+/**
+ * WebViewPage
+ * @flow
+ **/
+'use strict'
+import React, {Component} from 'react'
+import {
+    Image,
+    ScrollView,
+    StyleSheet,
+    WebView,
+    Platform,
+    TouchableOpacity,
+    Text,
+    View,
+} from 'react-native'
+
+import NavigationBar from './NavigationBar'
+import ViewUtil from '../../utils/ViewUtil'
+const WEBVIEW_REF = 'webview';
+
+
+export default class WebViewPage extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: this.props.url,
+            canGoBack: false,
+            title: this.props.title,
+            theme:this.props.theme
+        }
+    }
+
+    onBackPress(e) {
+        if (this.state.canGoBack) {
+            this.refs[WEBVIEW_REF].goBack();
+        } else {
+            this.props.navigator.pop();
+        }
+    }
+
+    onNavigationStateChange(navState) {
+        this.setState({
+            canGoBack: navState.canGoBack,
+            url: navState.url,
+        });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <NavigationBar
+                    navigator={this.props.navigator}
+                    popEnabled={false}
+                    style={{backgroundColor:'#3EA4FF'}}
+                    statusBar={{backgroundColor:'#3EA4FF'}}
+                    leftButton={ViewUtil.getLeftButton(()=>this.onBackPress())}
+                    title={this.state.title}
+                />
+                <WebView
+                    ref={WEBVIEW_REF}
+                    startInLoadingState={true}
+                    onNavigationStateChange={(e)=>this.onNavigationStateChange(e)}
+                    source={{uri: this.props.url}}/>
+            </View>
+
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+    },
+})
